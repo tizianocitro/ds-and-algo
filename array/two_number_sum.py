7b726f7a2d5347e9054293cd124cfb31a65d0569
Tiziano <tizianocitro98@gmail.com>
2023-07-04 13:44:13 +0200

    Added two number sum

---
+++ array/two_number_sum.py
@@ -0,0 +1,41 @@
+# Write a function that takes in a non-empty array of distinct integers and an integer representing a target sum.
+# If any two numbers in the input array sum up to the target sum, the function should return them in an array, in any order.
+# If no two numbers sum up to the target sum, the function should return an empty array.
+# Note that the target sum has to be obtained by summing two different integers in the array; you can't add a single integer to itself in order to obtain the target sum.
+# You can assume that there will be at most one pair of numbers summing up to the target sum.
+
+# Input:
+# array = [3, 5, -4, 8, 11, 1, -1, 6]
+# targetSum = 10
+# Output: [-1, 11]
+
+# solution one with set
+# Complexity
+# O(n) time - where n is the number of elements in the array
+# O(n) space - because we create a set wchihc in the worst case will contain n numbers where n is the number of elements in the array
+def two_number_sum(array, target_sum):
+    complements = set()
+    for n in array:
+        complement = target_sum - n
+        if n in complements:
+            return [n, complement]
+        complements.add(complement)
+    return []
+
+# solution two with the use of two pointers
+# Complexity
+# O(n) time - where n is the number of elements in the array
+# O(1) space
+def two_number_sum(array, target_sum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while right >= left:
+        sum = array[left] + array[right]
+        if sum == target_sum:
+            return [array[left], array[right]]
+        if sum < target_sum:
+            left += 1
+        if sum > target_sum:
+            right -= 1
+    return []
