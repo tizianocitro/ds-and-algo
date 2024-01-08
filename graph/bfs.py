ee35177080f6eb09d7226ca4c7da46eb925d9289
Tiziano <tizianocitro98@gmail.com>
2024-01-08 13:33:34 +0100

    Added difficulty to graph problems

--- graph/bfs.py
+++ graph/bfs.py
@@ -1,3 +1,5 @@
+# !difficulty: medium
+
 # You're given a Node class that has a name and an array of optional children nodes. When put together, nodes form an acyclic tree-like structure.
 # Implement the breadthfirstSearch method on the Node class, which takes in an empty array, traverses the tree using the Breadth-first Search approach (specifically navigating the tree from left to right),
 # stores all of the nodes' names in the input array, and returns it.
