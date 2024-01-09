4159ae1d8f373c9d09a2ecbc0b430dbe1def6c9c
Tiziano <tizianocitro98@gmail.com>
2024-01-09 09:21:22 +0100

    Added difficulty to searching problems

--- searching/binary_search.py
+++ searching/binary_search.py
@@ -1,3 +1,5 @@
+# !difficulty: easy
+
 '''
 Write a function that takes in a sorted array of integers as well as a target integer.
 The function should use the Binary Search algorithm to determine if the target integer is contained in the array
