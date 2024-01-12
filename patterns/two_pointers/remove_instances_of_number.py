b045ddb2933b39047a1c17effcfec71ad6ae0cbd
Tiziano <tizianocitro98@gmail.com>
2024-01-12 18:41:49 +0100

    Solved remove instances of number

---
+++ patterns/two_pointers/remove_instances_of_number.py
@@ -0,0 +1,24 @@
+# !difficulty: easy
+
+'''
+Given an unsorted array of numbers and a target key,
+remove all instances of key in-place and return the new length of the array.
+
+Input: [3, 2, 3, 6, 3, 10, 9, 3], key = 3
+Output: 4 // The first four elements after removing every instance of key will be [2, 6, 10, 9]
+'''
+
+# solution one
+# Complexity:
+# O(n) time - where n is the size of the array
+# O(1) space
+class Solution:
+    def remove(self, arr, key):
+        nextElement = 0
+
+        for i in range(len(arr)):
+            if arr[i] != key:
+                arr[nextElement] = arr[i]
+                nextElement += 1
+
+        return nextElement
