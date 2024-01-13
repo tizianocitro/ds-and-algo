c4560a9ac287ee26570d8a45c3bb43a6e2fa727b
Tiziano <tizianocitro98@gmail.com>
2024-01-13 13:02:30 +0100

    Solved triplet sum to zero

---
+++ patterns/two_pointers/triplet_sum_to_zero.py
@@ -0,0 +1,39 @@
+# !difficulty: medium
+
+'''
+Given an array of unsorted numbers, find all unique triplets in it that add up to zero.
+
+Input: [-3, 0, 1, 2, -1, 1, -2]
+Output: [[-3, 1, 2], [-2, 0, 2], [-2, 1, 1], [-1, 0, 1]]
+'''
+
+# solution one
+# Complexity:
+# O(n^2) time - where n is the size of the array
+# O(n) space - where n is the size of the array
+class Solution:
+    def searchTriplets(self, arr):
+        triplets = []
+        arr.sort()
+        for i, n in enumerate(arr):
+            if i > 0 and n == arr[i - 1]:
+                continue
+            self.searchPair(arr, triplets, i + 1, len(arr) - 1, -n)
+        return triplets
+
+    def searchPair(self, arr, triplets, left, right, target):
+        while left <= right:
+            l, r = arr[left], arr[right]
+            sum = l + r
+            if sum == target:
+                triplets.append([l, r, -target])
+                left += 1
+                right -= 1
+                while left < right and arr[left] == arr[left - 1]:
+                    left += 1  # skip same element to avoid duplicate triplets
+                while left < right and arr[right] == arr[right + 1]:
+                    right -= 1  # skip same element to avoid duplicate triplets
+            if sum > target:
+                right -= 1
+            else:
+                left += 1
