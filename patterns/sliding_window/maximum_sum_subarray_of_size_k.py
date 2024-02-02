399750275de94984b92f7498c6434ebe561e0135
Tiziano <tizianocitro98@gmail.com>
2024-02-02 19:08:09 +0100

    Solved maximum sum subarray of size k

---
+++ patterns/sliding_window/maximum_sum_subarray_of_size_k.py
@@ -0,0 +1,26 @@
+# !difficulty: easy
+
+'''Problem:
+Given an array of positive numbers and a positive number k,
+find the maximum sum of any contiguous subarray of size k.
+
+Input: [2, 1, 5, 1, 3, 2], k = 3 
+Output: 9
+Explanation: Subarray with maximum sum is [5, 1, 3].
+'''
+
+# solution one
+# Complexity:
+# O(n) time - where n is the length of the input array
+# O(1) space
+class Solution:
+    def findMaxSumSubArray(self, k, arr):
+        maxSum, sum, start = 0, 0, 0
+        for end in range(len(arr)):
+            sum += arr[end]
+            if end >= k - 1:
+                maxSum = max(maxSum, sum)
+                sum -= arr[start]
+                start += 1
+        return maxSum
+    
