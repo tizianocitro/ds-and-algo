a9d7bf3e2824f3ec6252daa8db68258de5da813f
Tiziano <tizianocitro98@gmail.com>
2024-02-24 18:05:21 +0100

    Added in-place reversal of a linked list

---
+++ patterns/in-place_reversal_linkedlist/README.md
@@ -0,0 +1,7 @@
+# In-place Reversal of a Linked List
+
+In a lot of problems, we are asked to reverse the links between a set of nodes of a LinkedList. Often, the constraint is that we need to do this in-place, i.e., using the existing node objects and without using extra memory.
+
+In-place Reversal of a LinkedList pattern describes an efficient way to solve the above problem. Below an example of the application of the pattern:
+
+![Visual representation of in-place Reversal of a LinkedList](https://github.com/tizianocitro/ds-and-algo/blob/main/assets/reverse_linkedlist.png "Visual representation of in-place Reversal of a LinkedList")
---
+++ patterns/in-place_reversal_linkedlist/reverse_linkedlist.py
@@ -0,0 +1,39 @@
+# !difficulty: easy
+
+'''Problem:
+Given the head of a Singly LinkedList, reverse the LinkedList. Write a function to return the new head of the reversed LinkedList.
+
+Input: 1 -> 2 -> 3 -> 4 -> 5 -> None
+Output: 5 -> 4 -> 3 -> 2 -> 1 -> None
+'''
+
+# solution one
+# Complexity:
+# 0(n) time - where n is the number of nodes in the linked list
+# 0(1) space
+class Node:
+    def __init__(self, value, next=None):
+        self.val = value
+        self.next = next
+
+class Solution:
+    def reverse(self, head):
+        prev = None
+        # with 1 -> 2 -> 3 -> 4 -> 5 -> None,
+        # at first iteration, head starts as 1
+        # at second iteration, head becomes 2
+        while head is not None:
+            # at first iteration, next is 2
+            # at second iteration, next is 3
+            next = head.next
+            # at first iteration, head is 1 and head.next becomes None instead of 2
+            # at second iteration, head is 2 and head.next becomes 1 instead of 3
+            head.next = prev
+            # at first iteration, prev becomes 1
+            # at second iteration, prev becomes 2
+            prev = head
+            # at first iteration, head becomes 2
+            # at second iteration, head becomes 3
+            head = next
+        # after all iterations, prev becomes 5, which is the new head of the reversed linked list
+        return prev
