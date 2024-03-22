27a26dcb8c6df70d70879379c2ed92c6e2dd0658
Tiziano <tizianocitro98@gmail.com>
2024-03-22 19:16:12 +0100

    feat: added new solution to binary tree path sum

--- patterns/tree_depth_first_search/binary_tree_path_sum.py
+++ patterns/tree_depth_first_search/binary_tree_path_sum.py
@@ -8,7 +8,32 @@ Output: True
 Explanation: There is a path from the root to the leaf with a sum of 23 (12 -> 1 -> 10).
 '''
 
-# solition one with recursion
+# solition one with recursion and no sum variable value change
+# Complexity:
+# O(n) time - where n is the number of nodes in the tree
+# O(n) space - where n is the number of nodes in the tree because,
+# in the worst case, the recursive call stack will be equal to the height of the tree
+# the worst case will happen when the given tree is a linked list (i.e., every node has only one child)
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPath(self, root, sum):
+        if root is None:
+            return False
+        
+        # if the current node is a leaf and its value is equal to the sum, we've found a path
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+
+        # recursively call to traverse the left and right sub-tree
+        # return true if any of the two recursive call return true
+        return self.hasPath(root.left, sum - root.val) or self.hasPath(root.right, sum - root.val)
+
+# solition two with recursion and sum variable value change
 # Complexity:
 # O(n) time - where n is the number of nodes in the tree
 # O(n) space - where n is the number of nodes in the tree because,
