91027c0e84fa51132414969fbdf603523d92eeff
Tiziano <tizianocitro98@gmail.com>
2024-03-22 16:10:41 +0100

    feat: solved binary tree path sum

---
+++ patterns/tree_depth_first_search/binary_tree_path_sum.py
@@ -0,0 +1,38 @@
+# !difficulty: easy
+
+'''Problem:
+Given a binary tree and a number S, find if the tree has a path from root-to-leaf such that the sum of all the node values of that path equals S.
+
+Input: [12, 7, 1, 9, None, 10, 5], S = 23
+Output: True
+Explanation: There is a path from the root to the leaf with a sum of 23 (12 -> 1 -> 10).
+'''
+
+# solition one with recursion
+# Complexity:
+# O(n) time - where n is the number of nodes in the tree
+# O(n) space - where n is the number of nodes in the tree because,
+# in the worst case, the recursive call stack will be equal to the height of the tree
+# the worst case will happen when the given tree is a linked list (i.e., every node has only one child)
+class TreeNode:
+    def __init__(self, val, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def hasPath(self, root, sum):
+        if root is None:
+            return False
+        
+        # if the current node is a leaf and its value is equal to the sum, we've found a path
+        if root.val == sum and root.left is None and root.right is None:
+            return True
+
+        # subtract the value of the current node from the sum,
+        # in this way, it will be reduced fro when it reaches a leaf
+        sum -= root.val
+
+        # recursively call to traverse the left and right sub-tree
+        # return true if any of the two recursive call return true
+        return self.hasPath(root.left, sum) or self.hasPath(root.right, sum)
