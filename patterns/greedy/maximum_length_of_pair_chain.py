e329e0aab23f5db600c570e53ed04e1fec66186c
Tiziano <tizianocitro98@gmail.com>
2024-06-13 19:52:54 +0200

    fix: fixed problem for maximum length of pair chain

--- patterns/greedy/maximum_length_of_pair_chain.py
+++ patterns/greedy/maximum_length_of_pair_chain.py
@@ -6,16 +6,16 @@ A pair [a, b] can follow another pair [c, d] in the chain if b < c.
 You can select pairs in any order and don't need to use all the given pairs.
 
 Input: [[1,2], [3,4], [2,3]]
-Expected Output: 2
-Explaination: The longest chain is [1,2] -> [3,4]. The chain [1,2] -> [2,3] is invalid because 2 is not smaller than 2.
+Output: 2
+Explanation: The longest chain is [1,2] -> [3,4]. The chain [1,2] -> [2,3] is invalid because 2 is not smaller than 2.
 
 Input: [[5,6], [1,2], [8,9], [2,3]]
-Expected Output: 3
-Explaination: The chain can be [1,2] -> [5,6] -> [8,9] or [2,3] -> [5,6] -> [8, 9].
+Output: 3
+Explanation: The chain can be [1,2] -> [5,6] -> [8,9] or [2,3] -> [5,6] -> [8, 9].
 
 Input: [[7,8], [5,6], [1,2], [3,5], [4,5], [2,3]]
-Expected Output: 3
-Explaination: The longest possible chain is formed by chaining [1,2] -> [3,5] -> [7,8].
+Output: 3
+Explanation: The longest possible chain is formed by chaining [1,2] -> [3,5] -> [7,8].
 '''
 
 # solution one
